799c7418ff534eba0a3839117d21c65a6a245b73
sstoudenmier <sethstoudenmier@gmail.com>
2017-06-15 21:24:13 -0400

    Changed functionality
    Added additional functionality to create an account with out searching
    to update, update an email address, and update a phone number.

--- server.js
+++ server.js
@@ -9,12 +9,62 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post('/update', function(req, res) {
+app.post('/createContact', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
+        // attempt the phone number of a salesforce contact
+        conn.query(
+            'INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
+            [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+            function(err, result) {}
+                done();
+                res.json(result);
+            }
+        );
+    });
+});
+
+app.post('/updatePhone', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        // attempt the phone number of a salesforce contact
         conn.query(
             'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
+            [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+            function(err, result) {
+                if (err != null || result.rowCount == 0) {
+                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
+                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+                  function(err, result) {
+                    done();
+                    if (err) {
+                        res.status(400).json({error: err.message});
+                    }
+                    else {
+                        // this will still cause jquery to display 'Record updated!'
+                        // eventhough it was inserted
+                        res.json(result);
+                    }
+                  });
+                }
+                else {
+                    done();
+                    res.json(result);
+                }
+            }
+        );
+    });
+});
+
+app.post('/updateEmail', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        // attempt the phone number of a salesforce contact
+        conn.query(
+            'UPDATE salesforce.Contact SET Email = $4 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND $1 = Phone',
             [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
